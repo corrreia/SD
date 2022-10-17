a3cbe3874970fe198247f891f2b26d62240a171a
corrreia <tomasc2002@gmail.com>
2022-10-17 10:51:12 +0100

    added project2

---
+++ proj2/client/client_stub.h
@@ -0,0 +1,59 @@
+#ifndef _CLIENT_STUB_H
+#define _CLIENT_STUB_H
+
+#include "data.h"
+#include "entry.h"
+
+/* Remote tree. A definir pelo grupo em client_stub-private.h
+ */
+struct rtree_t;
+
+/* Função para estabelecer uma associação entre o cliente e o servidor, 
+ * em que address_port é uma string no formato <hostname>:<port>.
+ * Retorna NULL em caso de erro.
+ */
+struct rtree_t *rtree_connect(const char *address_port);
+
+/* Termina a associação entre o cliente e o servidor, fechando a 
+ * ligação com o servidor e libertando toda a memória local.
+ * Retorna 0 se tudo correr bem e -1 em caso de erro.
+ */
+int rtree_disconnect(struct rtree_t *rtree);
+
+/* Função para adicionar um elemento na árvore.
+ * Se a key já existe, vai substituir essa entrada pelos novos dados.
+ * Devolve 0 (ok, em adição/substituição) ou -1 (problemas).
+ */
+int rtree_put(struct rtree_t *rtree, struct entry_t *entry);
+
+/* Função para obter um elemento da árvore.
+ * Em caso de erro, devolve NULL.
+ */
+struct data_t *rtree_get(struct rtree_t *rtree, char *key);
+
+/* Função para remover um elemento da árvore. Vai libertar 
+ * toda a memoria alocada na respetiva operação rtree_put().
+ * Devolve: 0 (ok), -1 (key not found ou problemas).
+ */
+int rtree_del(struct rtree_t *rtree, char *key);
+
+/* Devolve o número de elementos contidos na árvore.
+ */
+int rtree_size(struct rtree_t *rtree);
+
+/* Função que devolve a altura da árvore.
+ */
+int rtree_height(struct rtree_t *rtree);
+
+/* Devolve um array de char* com a cópia de todas as keys da árvore,
+ * colocando um último elemento a NULL.
+ */
+char **rtree_get_keys(struct rtree_t *rtree);
+
+/* Devolve um array de void* com a cópia de todas os values da árvore,
+ * colocando um último elemento a NULL.
+ */
+void **rtree_get_values(struct rtree_t *rtree);
+
+
+#endif
---
+++ proj2/server/tree_skel.h
@@ -0,0 +1,24 @@
+#ifndef _TREE_SKEL_H
+#define _TREE_SKEL_H
+
+#include "sdmessage.pb-c.h"
+#include "tree.h"
+
+/* Inicia o skeleton da árvore.
+ * O main() do servidor deve chamar esta função antes de poder usar a
+ * função invoke(). 
+ * Retorna 0 (OK) ou -1 (erro, por exemplo OUT OF MEMORY)
+ */
+int tree_skel_init();
+
+/* Liberta toda a memória e recursos alocados pela função tree_skel_init.
+ */
+void tree_skel_destroy();
+
+/* Executa uma operação na árvore (indicada pelo opcode contido em msg)
+ * e utiliza a mesma estrutura message_t para devolver o resultado.
+ * Retorna 0 (OK) ou -1 (erro, por exemplo, árvore nao incializada)
+*/
+int invoke(struct message_t *msg);
+
+#endif
