be4d222315bdd85bde3669c9b5ed26130bd3d1a8
corrreia <tomasc2002@gmail.com>
2022-11-04 10:55:12 +0000

    changes to .h files struct _MessageT to MessageT)

--- proj2/server/include/network_server.h
+++ proj2/server/include/network_server.h
@@ -23,14 +23,14 @@ int network_main_loop(int listening_socket);
  * - De-serializar estes bytes e construir a mensagem com o pedido,
  *   reservando a memória necessária para a estrutura message_t.
  */
-struct _MessageT *network_receive(int client_socket);
+MessageT *network_receive(int client_socket);
 
 /* Esta função deve:
  * - Serializar a mensagem de resposta contida em msg;
  * - Libertar a memória ocupada por esta mensagem;
  * - Enviar a mensagem serializada, através do client_socket.
  */
-int network_send(int client_socket, struct _MessageT *msg);
+int network_send(int client_socket, MessageT *msg);
 
 /* A função network_server_close() liberta os recursos alocados por
  * network_server_init().
