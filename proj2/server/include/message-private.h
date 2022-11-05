258c43a0a3d52cced8f1e9cb35fbb9e1a74b1720
corrreia <tomasc2002@gmail.com>
2022-11-05 17:01:46 +0000

    proj1 branch

--- proj2/server/include/message-private.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MESSAGE_PRIVATE_H
-#define _MESSAGE_PRIVATE_H
-
-#include "sdmessage.pb-c.h"
-
-int read_all(int sock, uint8_t *buf, int len);
-int write_all(int sock, uint8_t *buf, int len);
-
-#endif
